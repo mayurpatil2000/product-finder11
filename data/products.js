75932ed584fd56555a24776cd9816cf9a5d36b7e
Mayur Patil <mayurpatil65953@gmail.com>
2024-01-21 10:31:31 +0530

    first commit

---
+++ data/products.js
@@ -0,0 +1,181 @@
+var products = [
+  {
+    permalink: "https://www.amazon.in/Apple-iPhone-14-512GB-Blue/dp/B0BDJH3V3Q",
+    title: "Apple iPhone 14",
+    amount: 449,
+    category: "mobile",
+    performance: "6gb",
+    description:
+      "17.00 cm (6.7″) or 15.54 cm (6.1″)Super Retina XDR display1ProMotion technologyAlways-On display, large 4850mAh battery A17 Pro chipwith 6-core GPU",
+    image:
+      "https://m.media-amazon.com/images/I/31VjlrbE3bL._SY445_SX342_QL70_FMwebp_.jpg?width=220&height=220",
+    // image:
+    //   "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-2-202309_GEO_EMEA?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1693010532692",
+  },
+  {
+    permalink: "https://www.amazon.in/dp/B0BDJH6GL8",
+    title: "iPhone 14 Pro Max- Gold",
+    amount: 289,
+    category: "mobiles",
+    performance: "8gb",
+    description:
+      "17.00 cm (6.7-inch) Super Retina XDR display featuring Always-On and ProMotion A16 Bionic, the ultimate smartphone chip. Superfast 5G cellular",
+    image:
+      "https://m.media-amazon.com/images/I/71T5NVOgbpL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink: "https://www.amazon.in/dp/B0BDK62STN",
+    title: "Apple iPhone 14 Plus",
+    amount: 237,
+    category: "mobiles",
+    performance: "4gb",
+    description:
+      "16.95 cm (6.7-inch) Super Retina XDR display Advanced camera system for better photos in any light A15 Bionic chip with 5-core GPU for lightning-fast performance. Superfast 5G cellular",
+    image:
+      "https://m.media-amazon.com/images/I/61BGE6iu4AL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink: "https://www.amazon.in/dp/B09V44MF6K",
+    title: "Apple iPhone SE",
+    amount: 101,
+    category: "mobiles",
+    performance: "4gb",
+    description:
+      "11.94 cm (4.7-inch) Retina HD display Advanced single camera system with 12MP Wide camera Smart Photographic Styles, Portrait mode, and 4K video up to 60 fp A15 Bionic chip for lightning-fast performance",
+    image:
+      "https://m.media-amazon.com/images/I/61TOWf11+jL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink: "https://www.amazon.in/dp/B09V4MXBSN",
+    title: "iPhone 13",
+    amount: 201,
+    category: "mobiles",
+    performance: "6gb",
+    description:
+      "15 cm (6.1-inch) Super Retina XDR display Cinematic mode adds shallow depth of field and shifts focusautomatically in your videos A15 Bionic chip for lightning-fast performance      ",
+    image:
+      "https://m.media-amazon.com/images/I/61-r9zOKBCL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/dp/B08L5WHFT9?ref=ods_ucc_kindle_B08L5WHFT9_nrc_ucc",
+    title: " iPhone 12 ",
+    amount: 351,
+    category: "mobiles",
+    performance: "4gb",
+    description: "stainless steel frame and 12.6cm (5) IPS Full HD display.",
+    image:
+      "https://m.media-amazon.com/images/I/71E5zB1qbIL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/Apple-iPhone-15-512-GB/dp/B0CHX9MF1L?ref_=ast_sto_dp&th=1&psc=1",
+    title: "iPhone 15",
+    amount: 359,
+    category: "mobiles",
+    performance: "8gb",
+    description:
+      "DYNAMIC ISLAND COMES TO IPHONE 15 — Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else. You can see who’s calling, track your next ride, check your flight status, and so much more.",
+    image:
+      "https://m.media-amazon.com/images/I/71657TiFeHL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/Apple-iPhone-15-Plus-512/dp/B0CHX8CK97?ref_=ast_sto_dp&th=1&psc=1",
+    title: "iPhone 15 Plus",
+    amount: 199,
+    category: "mobiles",
+    performance: "6gb",
+    description:
+      "Snapdragon 410 64-bit / 4G Dual SIM / 11.9cm (4.7) IPS HD display / 8MP camera f/2.2",
+    image:
+      "https://m.media-amazon.com/images/I/713d2it9mhL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/Apple-Smartwatch-Product-Aluminum-Resistant/dp/B0CHXZJ4PX?ref_=ast_sto_dp&th=1&psc=1",
+    title: "Apple Watch Series 9 [GPS 41mm] Smartwatch",
+    amount: 303,
+    category: "smart",
+    description:
+      "Apple Watch Series 9 [GPS 41mm] Smartwatch with (Product) RED Aluminum Case with (Product) RED Sport Band M/L. Fitness Tracker, Blood Oxygen & ECG Apps, Always-On Retina Display, Water Resistant",
+    image:
+      "https://m.media-amazon.com/images/I/71QX7TwoNWL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/Apple-Smartwatch-Starlight-Aluminum-Detection/dp/B0CHY29TPH?ref_=ast_sto_dp&th=1&psc=1",
+    title: "Apple Watch SE ",
+    amount: 433,
+    category: "smart",
+    description:
+      "Apple Watch SE (2nd Gen, 2023) [GPS 40mm] Smartwatch with Starlight Aluminum Case with Starlight Sport Band M/L. Fitness & Sleep Tracker, Crash Detection, Heart Rate Monitor, Retina Display    ",
+    image:
+      "https://m.media-amazon.com/images/I/61ty3NGrdNL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/Apple-AirPods-with-Charging-Case/dp/B07Q6153FQ?ref_=ast_sto_dp",
+    title: "Apple AirPods",
+    amount: 449,
+    category: "smart",
+    description:
+      "Automatically on, automatically connected Easy setup for all your Apple devices Quick access to Siri by saying “Hey SiriDouble-tap to play or skip forward",
+    image:
+      "https://m.media-amazon.com/images/I/7120GgUKj3L._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink: "https://www.amazon.in/dp/B08Q2SKR3B",
+    title: "AirPods Max",
+    amount: 499,
+    category: "smart",
+    description:
+      "Apple-designed dynamic driver provides high-fidelity audio Active Noise Cancellation blocks outside noise, so you can immerse yourself in music Transparency mode for hearing and interacting with the world around you Spatial audio with dynamic head tracking provides theater-like sound that surrounds you",
+    image:
+      "https://m.media-amazon.com/images/I/81thV7SoLZL._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/Apple-MX532ZMA-New-AirTag/dp/B0935DN1BN?ref_=ast_sto_dp&th=1&psc=1",
+    title: "Apple AirTag",
+    amount: 299,
+    category: "accessories",
+    description:
+      "Keep track of and find your items alongside friends and devices in the Find My app Simple one-tap setup instantly connects AirTag with your iPhone or iPadPlay a sound on the built-in speaker to help find your things, or just ask Siri for help",
+    image:
+      "https://m.media-amazon.com/images/I/71L5lSfJIPS._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/2022-Apple-TV-4K-Wi%E2%80%91Fi-with-64GB-Storage-3rd-Generation/dp/B0BJMP7DRF?ref_=ast_sto_dp",
+    title: "Apple TV 4K",
+    amount: 139,
+    category: "accessories",
+    description:
+      "4K Dolby Vision and HDR10+ for vivid picture quality Dolby Atmos for three-dimensional, theater-like sound Incredible power with the A15 Bionic chip",
+    image:
+      "https://m.media-amazon.com/images/I/616bgkA2J0L._SL1500_.jpg?width=220&height=220",
+  },
+  {
+    permalink:
+      "https://www.amazon.in/Apple-24-inch-8%E2%80%91core-Unified-Memory/dp/B0CM5HD9NS?ref_=ast_sto_dp&th=1&psc=1",
+    title: "Apple 2023 iMac",
+    amount: 249,
+    category: "accessories",
+    description:
+      "SUPERCHARGED BY M3 — Get more done faster with a next-generation chip from Apple. From creating presentations to gaming, you’ll fly through work and play.FITS PERFECTLY IN YOUR SPACE — The all-in-one desktop design is strikingly thin, comes in vibrant colours and becomes the centrepiece of any room.",
+    image:
+      "https://m.media-amazon.com/images/I/61KBIj28ZUL._SL1500_.jpg?width=220&height=220",
+  },
+
+  {
+    permalink:
+      "https://www.amazon.in/Apple-Original-MMTN2ZM-Lightning-Connector/dp/B01M1EEPOB/ref=pd_rhf_dp_s_pd_crcd_d_sccl_2_3/262-4865460-3387206?pd_rd_w=17hbo&content-id=amzn1.sym.785b16db-ca40-46a3-ae75-2b38bb48d1aa&pf_rd_p=785b16db-ca40-46a3-ae75-2b38bb48d1aa&pf_rd_r=XS4221Q4WWK6FBE9PFXK&pd_rd_wg=FnoJO&pd_rd_r=6ac3d034-fa0f-4a12-b130-eb8540ab8a0a&pd_rd_i=B01M1EEPOB&psc=1",
+    amount: 44,
+    category: "accessories",
+    description:
+      "The speakers inside the EarPods have been engineered to maximize sound output and minimize sound loss, which means you get high-quality audio.The EarPods with Lightning Connector also include a built-in remote that lets you adjust the volume, control the playback of music and video, and answer or end calls with a pinch of the cord.",
+    image:
+      "https://m.media-amazon.com/images/I/418AP8pw3KL._SL1500_.jpg?width=220&height=220",
+  },
+];
